32813c09889a3586f26c81689a5bdf62441cec1e
DalatCoder <hieuntctk42@gmail.com>
2020-10-07 14:02:29 +0700

    Bo sung chuc nang sua thong itn

--- Bai01_Trang60/BT_Page60_Buoi3/FormNhanVien.cs
+++ Bai01_Trang60/BT_Page60_Buoi3/FormNhanVien.cs
@@ -112,6 +112,39 @@ namespace BT_Page60_Buoi3
 
         private void btnSua_Click(object sender, EventArgs e)
         {
+            var selectedItem = listView1.SelectedItems[0];
+
+            //row.SubItems.Add(item.TenNV);
+            //row.SubItems.Add(item.NgaySinh.ToString("dd/MM/yyyy"));
+            //row.SubItems.Add(item.GioiTinh);
+            //row.SubItems.Add(item.DiaChi);
+            //row.SubItems.Add(item.Email);
+            //row.SubItems.Add(item.SoDT);
+            //row.SubItems.Add(item.PhongBan);
+            //row.SubItems.Add(item.Hinh);
+
+            string maNV = selectedItem.SubItems[0].Text;
+            string tenNV = selectedItem.SubItems[1].Text;
+            string[] ngaySinhArr = selectedItem.SubItems[2].Text.Split('/');
+            string gioiTinh = selectedItem.SubItems[3].Text;
+            string diaChi = selectedItem.SubItems[4].Text;
+            string email = selectedItem.SubItems[5].Text;
+            string soDT = selectedItem.SubItems[6].Text;
+            string phongBan = selectedItem.SubItems[7].Text;
+            string hinh = selectedItem.SubItems[8].Text;
+
+            DateTime ngaySinh = new DateTime(int.Parse(ngaySinhArr[2]), int.Parse(ngaySinhArr[1]), int.Parse(ngaySinhArr[0]));
+
+            mtbMaNV.Text = maNV;
+            txtName.Text = tenNV;
+            if (gioiTinh == "Nam") rdNam.Checked = true;
+            else rdNu.Checked = true;
+            dtpNgaySinh.Value = ngaySinh;
+            txtAddress.Text = diaChi;
+            txtMail.Text = email;
+            txtHinh.Text = hinh;
+            cboPhongBan.Text = phongBan;
+            mtbSoDT.Text = soDT;
         }
     }
 }
