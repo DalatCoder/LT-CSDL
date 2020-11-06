a75ccabefc0eefb623b10a06f02784afc5cb70b8
Trong Hieu <hieuntctk42@gmail.com>
2020-11-06 21:37:58 +0700

    Lấy danh sách 👻👻👻

---
+++ Lab06_Basic_Command/Lab06_Basic_Command/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Lab06_Basic_Command
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnLoad = new System.Windows.Forms.Button();
+			this.lvCategory = new System.Windows.Forms.ListView();
+			this.chID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.chType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.SuspendLayout();
+			// 
+			// btnLoad
+			// 
+			this.btnLoad.Location = new System.Drawing.Point(238, 12);
+			this.btnLoad.Name = "btnLoad";
+			this.btnLoad.Size = new System.Drawing.Size(173, 23);
+			this.btnLoad.TabIndex = 0;
+			this.btnLoad.Text = "Lấy danh sách";
+			this.btnLoad.UseVisualStyleBackColor = true;
+			this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+			// 
+			// lvCategory
+			// 
+			this.lvCategory.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chID,
+            this.chName,
+            this.chType});
+			this.lvCategory.FullRowSelect = true;
+			this.lvCategory.Location = new System.Drawing.Point(12, 80);
+			this.lvCategory.MultiSelect = false;
+			this.lvCategory.Name = "lvCategory";
+			this.lvCategory.Size = new System.Drawing.Size(628, 358);
+			this.lvCategory.TabIndex = 1;
+			this.lvCategory.UseCompatibleStateImageBehavior = false;
+			this.lvCategory.View = System.Windows.Forms.View.Details;
+			// 
+			// chID
+			// 
+			this.chID.Text = "Mã Loại";
+			this.chID.Width = 158;
+			// 
+			// chName
+			// 
+			this.chName.Text = "Tên loại món ăn";
+			this.chName.Width = 325;
+			// 
+			// chType
+			// 
+			this.chType.Text = "Loại";
+			this.chType.Width = 139;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(652, 450);
+			this.Controls.Add(this.lvCategory);
+			this.Controls.Add(this.btnLoad);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnLoad;
+		private System.Windows.Forms.ListView lvCategory;
+		private System.Windows.Forms.ColumnHeader chID;
+		private System.Windows.Forms.ColumnHeader chName;
+		private System.Windows.Forms.ColumnHeader chType;
+	}
+}
+
---
+++ Lab06_Basic_Command/Lab06_Basic_Command/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab06_Basic_Command
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnLoad_Click(object sender, EventArgs e)
+		{
+			string connectionString = "server=.; database = RestaunrantManagement; Integrated Security = true; ";
+
+			SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+			string query = "SELECT ID, NAME, TYPE FROM Category";
+
+			SqlCommand sqlCommand = sqlConnection.CreateCommand();
+			sqlCommand.CommandText = query;
+
+			sqlConnection.Open();
+
+			SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+			DisplayCategory(sqlDataReader);
+
+			sqlConnection.Close();
+		}
+
+		private void DisplayCategory(SqlDataReader reader)
+		{
+			lvCategory.Items.Clear();
+
+			while (reader.Read())
+			{
+				ListViewItem item = new ListViewItem(reader["ID"].ToString());
+
+				item.SubItems.Add(reader["Name"].ToString());
+				item.SubItems.Add(reader["Type"].ToString());
+
+				lvCategory.Items.Add(item);
+			}
+		}
+	}
+}
