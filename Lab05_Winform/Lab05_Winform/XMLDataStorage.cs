485e9988ecc96c39b9946d885909224b2135627f
Trong Hieu <hieuntctk42@gmail.com>
2020-11-09 17:08:51 +0700

    XML DataStorage

---
+++ Lab05_Winform/Lab05_Winform/XMLDataStorage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Lab05_Winform
+{
+	public class XMLDataStorage : IStudentDataStorage
+	{
+		public string FilePath { get; }
+
+		public XMLDataStorage(string filePath)
+		{
+			FilePath = filePath;
+		}
+
+		public List<SinhVien> Load()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Write(List<SinhVien> sinhViens)
+		{
+			XDeclaration dec = new XDeclaration("1.0", "utf-8", "yes");
+
+		}
+	}
+}
