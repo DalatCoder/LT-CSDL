6d8e10b7d74a5c0373551245a2938b8cba1625f7
Trong Hieu <hieuntctk42@gmail.com>
2020-11-13 21:11:38 +0700

    Init commit 🚀🚀🚀

---
+++ Lab07_Advanced_Command_Execution/Lab07_Advanced_Command_Execution/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Lab07_Advanced_Command_Execution
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dgvFoodList = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
+			this.cboCategory = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.lblQuantity = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.lblCatName = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dgvFoodList)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dgvFoodList
+			// 
+			this.dgvFoodList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dgvFoodList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvFoodList.Location = new System.Drawing.Point(12, 69);
+			this.dgvFoodList.MultiSelect = false;
+			this.dgvFoodList.Name = "dgvFoodList";
+			this.dgvFoodList.Size = new System.Drawing.Size(776, 339);
+			this.dgvFoodList.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(142, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(102, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Chọn nhóm món ăn:";
+			// 
+			// cboCategory
+			// 
+			this.cboCategory.FormattingEnabled = true;
+			this.cboCategory.Location = new System.Drawing.Point(250, 10);
+			this.cboCategory.Name = "cboCategory";
+			this.cboCategory.Size = new System.Drawing.Size(248, 21);
+			this.cboCategory.TabIndex = 2;
+			this.cboCategory.SelectedIndexChanged += new System.EventHandler(this.cboCategory_SelectedIndexChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(121, 425);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(50, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Có tất cả";
+			// 
+			// lblQuantity
+			// 
+			this.lblQuantity.AutoSize = true;
+			this.lblQuantity.Location = new System.Drawing.Point(194, 425);
+			this.lblQuantity.Name = "lblQuantity";
+			this.lblQuantity.Size = new System.Drawing.Size(16, 13);
+			this.lblQuantity.TabIndex = 4;
+			this.lblQuantity.Text = "...";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(237, 425);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(101, 13);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "món ăn thuộc nhóm";
+			// 
+			// lblCatName
+			// 
+			this.lblCatName.AutoSize = true;
+			this.lblCatName.Location = new System.Drawing.Point(354, 425);
+			this.lblCatName.Name = "lblCatName";
+			this.lblCatName.Size = new System.Drawing.Size(16, 13);
+			this.lblCatName.TabIndex = 6;
+			this.lblCatName.Text = "...";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lblCatName);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.lblQuantity);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.cboCategory);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.dgvFoodList);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dgvFoodList)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dgvFoodList;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox cboCategory;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label lblQuantity;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label lblCatName;
+	}
+}
+
---
+++ Lab07_Advanced_Command_Execution/Lab07_Advanced_Command_Execution/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab07_Advanced_Command_Execution
+{
+	public partial class Form1 : Form
+	{
+		private DataTable foodTable;	
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			LoadCategory();
+		}
+
+		private void LoadCategory()
+		{
+			string connectionString = "server=.; database = RestaurantManagement; Integrated Security = true; ";
+			SqlConnection connection = new SqlConnection(connectionString);
+			SqlCommand command = connection.CreateCommand();
+
+			connection.Open();
+
+			command.CommandText = "SELECT ID, Name FROM Category";
+			SqlDataAdapter adapter = new SqlDataAdapter(command);
+			DataTable table = new DataTable("Category");
+
+			adapter.Fill(table);
+
+			cboCategory.DataSource = table;
+			cboCategory.DisplayMember = "Name";
+			cboCategory.ValueMember = "ID";
+
+			connection.Close();
+			connection.Dispose();
+		}
+
+		private void cboCategory_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (cboCategory.SelectedIndex == -1) return;
+
+			string connectionString = "server=.; database = RestaurantManagement; Integrated Security = true; ";
+			SqlConnection connection = new SqlConnection(connectionString);
+			SqlCommand command = connection.CreateCommand();
+
+			command.CommandText = "SELECT * FROM Food WHERE FoodCategoryID = @categoryId";
+			command.Parameters.Add("@categoryId", SqlDbType.Int);
+
+			if (cboCategory.SelectedValue is DataRowView)
+			{
+				DataRowView rowView = cboCategory.SelectedValue as DataRowView;
+				command.Parameters["@categoryId"].Value = rowView["ID"];
+			}
+			else
+			{
+				command.Parameters["@categoryId"].Value = cboCategory.SelectedValue;
+			}
+
+			SqlDataAdapter adapter = new SqlDataAdapter(command);
+			foodTable = new DataTable();
+
+			connection.Open();
+
+			adapter.Fill(foodTable);
+
+			connection.Close();
+			connection.Dispose();
+
+			dgvFoodList.DataSource = foodTable;
+
+			lblQuantity.Text = foodTable.Rows.Count.ToString();
+			lblCatName.Text = cboCategory.Text;
+		}
+	}
+}
